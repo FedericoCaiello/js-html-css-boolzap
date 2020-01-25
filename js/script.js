80f3bd5e8e9feb272f25a5f90360feb5d39c0828
Federico <federico.caiello@gmail.com>
2020-01-25 17:56:58 +0100

    input ricerca utenti

--- js/script.js
+++ js/script.js
@@ -24,8 +24,42 @@ function sendMessage () {
     console.log(newMessage);
 
     $('input.send-message').val('');
+
+    // Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+    setTimeout(function () {
+      var messaggioAmico = $('.template .message').clone();
+      console.log(messaggioAmico);
+
+      messaggioAmico.find('.message_text').text('okay');
+      messaggioAmico.find('.message_time').text(time);
+      messaggioAmico.addClass('received');
+      $('.main_background_img').append(messaggioAmico);
+      console.log(messaggioAmico);
+
+    }, 1000);
   }
 }
+
+$(document).ready(function () {
+  $('.nav_search_inline .fa-search').click(function () {
+    var ricerca = $('.nav_search').val();
+    var i = 0;
+    while (i < $('.contact-name').length) {
+      var nome = $('.contatti_list h1').eq(i).text();
+      var cancellaContatto = $('.contatti_list li').eq(i);
+      if ( nome.includes(ricerca)) {
+        cancellaContatto.show();
+        console.log('il nome esiste');
+      }
+      else {
+        cancellaContatto.hide();
+        console.log('il nome non ci sta');
+      }
+      console.log(nome);
+      i++;
+    }
+  });
+});
 
 function addZero(number) {
   if(number < 10) {
@@ -33,3 +67,8 @@ function addZero(number) {
   }
   return number;
 }
+
+
+
+// Milestone 2
+// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
